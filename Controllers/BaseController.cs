b30bd7168003abac9c0260c40db756e46a4157c4
Marcos Caetano <mcpestanajr@hotmail.com>
2022-07-17 16:52:18 -0300

    Adicionando método para gerenciar as regras de retorno, centralizando os codeHttp

---
+++ Controllers/BaseController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Estudo_Api_DDD.Controllers
+{
+    public class BaseController : Controller
+    {
+        protected ActionResult CustomResponse(object? result = null)
+        {
+            if (true)
+            {
+                return StatusCode(StatusCodes.Status200OK, new
+                {
+                    sucess = true,
+                    data = result
+                });
+            }
+
+            return StatusCode(StatusCodes.Status400BadRequest, new
+            {
+                sucess = false,
+                error = ""
+            });
+        }
+    }
+}
---
+++ Controllers/PacienteController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Estudo_Api_DDD.Controllers
+{
+    [Route("api/paciente")]
+    public class PacienteController : BaseController
+    {
+        [HttpPost]
+        public async Task<IActionResult> Create()
+        {
+            return CustomResponse();
+        }
+    }
+}
